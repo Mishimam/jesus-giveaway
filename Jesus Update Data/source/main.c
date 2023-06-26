5306456208e982a2097bd8c1a9c527308ecd64a8
Mishimam <111745275+Mishimam@users.noreply.github.com>
2023-06-26 22:52:14 +0200

    Update main.c

--- Jesus Update Data/source/main.c
+++ Jesus Update Data/source/main.c
@@ -1,8 +1,3 @@
-/*
-	Hello World example made by Aurelio Mannara for libctru
-	This code was modified for the last time on: 12/12/2014 21:00 UTC+1
-*/
-
 #include <3ds.h>
 #include <stdio.h>
 
@@ -14,18 +9,11 @@ int main(int argc, char **argv)
 	
 	gfxInitDefault();
 	
-	//In this example we need one PrintConsole for each screen
 	PrintConsole topScreen, bottomScreen;
 	
-	//Initialize console for both screen using the two different PrintConsole we have defined
 	consoleInit(GFX_TOP, &topScreen);
 	consoleInit(GFX_BOTTOM, &bottomScreen);
 
-	//Move the cursor to row 15 and column 19 and then prints "Hello World!"
-	//To move the cursor you have to print "\x1b[r;cH", where r and c are respectively
-	//the row and column where you want your cursor to move
-	//The top screen has 30 rows and 50 columns
-	//The bottom screen has 30 rows and 40 columns
 	consoleSelect(&topScreen);
 	printf("\x1b[15;11H\x1b[36mYou have received a free ps2!!!");
 	printf("\x1b[17;11H\x1b[36mAnd a free Wii U!!!");
@@ -40,13 +28,11 @@ int main(int argc, char **argv)
 	// Main loop
 	while (aptMainLoop())
 	{
-		//Scan all the inputs. This should be done once for each frame
 		hidScanInput();
 
-		//hidKeysDown returns information about which buttons have been just pressed (and they weren't in the previous frame)
 		u32 kDown = hidKeysDown();
 
-		if (kDown & KEY_START) break; // break in order to return to hbmenu
+		if (kDown & KEY_START) break; 
 		
 		bool didit = false;
 		static MiiSelectorConf msConf;
@@ -58,10 +44,6 @@ int main(int argc, char **argv)
 		{
 			consoleClear();
 			didit = true;
-			// Ensure that the config is initalized and that all
-			// defaults are set properly. (failing to do this to a new
-			// MiiSelecorConf at least once before launching the miiSelector
-			// can result in undefined behavior)
 			miiSelectorInit(&msConf);
 			miiSelectorLaunch(&msConf, &msRet);
 		}
@@ -70,31 +52,22 @@ int main(int argc, char **argv)
 		{
 			didit = true;
 			miiSelectorInit(&msConf);
-			// Sets title of Mii selector.
 			miiSelectorSetTitle(&msConf, "Sex Selector");
-			// Choose and option to enable. Any options not listed will be disabled.
-			// 0 can be used to select no option.
 			miiSelectorSetOptions(&msConf, MIISELECTOR_CANCEL|MIISELECTOR_GUESTS|MIISELECTOR_TOP|MIISELECTOR_GUESTSTART);
-			// Start on the Mii with the database index of 1.
-			// This is a guest Mii since the option MIISELECTOR_GUESTSTART is enabled.
-			// If MIISELECTOR_GUESTSTART was not enabled, this would be a user Mii.
 			miiSelectorSetInitialIndex(&msConf, 1);
-			// Blacklist main user Mii
 			miiSelectorBlacklistUserMii(&msConf, 0);
 			miiSelectorLaunch(&msConf, &msRet);
 		}
 		
-		if (kDown & KEY_X) // If the A button got pressed, start the app launch
+		if (kDown & KEY_X)
 		{
-			aptSetChainloader(0x0004001000022400LL, 0); // *EUR* camera app title ID
+			aptSetChainloader(0x0004001000022400LL, 0);
 			break;
 		}
 		
 		consoleSelect(&bottomScreen);
 		if (didit)
 		{
-			// Check that the data in the miiSelector return buffer
-			// is correct.
 			if (miiSelectorChecksumIsValid(&msRet))
 			{
 				if (!msRet.no_mii_selected)
@@ -109,8 +82,6 @@ int main(int argc, char **argv)
 					printf("Sex: %d\n", msRet.mii.mii_details.sex);
 					printf("Color: %d\n", msRet.mii.mii_details.shirt_color);
 					printf("Favorite: %d\n", msRet.mii.mii_details.favorite);
-					// Keep in mind that not all things align with their order in Mii Maker.
-					// https://www.3dbrew.org/wiki/Mii#Mii_values
 					printf("Eyebrow: %d\n", msRet.mii.eyebrow_details.style);
 					printf("Eyebrow color: %d\n", msRet.mii.eyebrow_details.color);
 					printf("Nose: %d\n", msRet.mii.nose_details.style);
@@ -120,11 +91,9 @@ int main(int argc, char **argv)
 				printf("Return checksum invalid.\n");
 		}
 
-		// Flush and swap framebuffers
 		gfxFlushBuffers();
 		gfxSwapBuffers();
 
-		//Wait for VBlank
 		gspWaitForVBlank();
 	}
 
